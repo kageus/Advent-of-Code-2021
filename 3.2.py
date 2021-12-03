bc13b906aabdd26523d66a0ff7030f7e31131b1c
Will Stevens <kageus@gmail.com>
2021-12-03 02:44:22 -0500

    spacing edit

--- 3.2.py
+++ 3.2.py
@@ -6,11 +6,11 @@ lineList = []
 for line in dataInput:
     lineList.append(str(line))
 
-bitCount = len(lineList[0])
-lineCount = len(lineList)
+bitCount    = len(lineList[0])
+lineCount   = len(lineList)
 
-oxygenRating = oxygen.oxygenRate(lineList, 0)
-carbonDioxRating = carbondiox.carbonDioxRate(lineList, 0)
+oxygenRating        = oxygen.oxygenRate(lineList, 0)
+carbonDioxRating    = carbondiox.carbonDioxRate(lineList, 0)
 
 def binaryStringToInt(binaryString):
     return int(f"0b{binaryString}", 2)
